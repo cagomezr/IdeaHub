3998f3e91fcd383ee478ce9e56034d1af4de008f
cagomezr <carlos.a.gomez.romero@gmail.com>
2017-12-21 17:30:06 -0500

    Carbon class added to seeder
    We added so  the  timestamps  seem proper

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class CommentsTableSeeder extends Seeder
 {
@@ -16,19 +17,22 @@ class CommentsTableSeeder extends Seeder
             'user_id' => 2,
 			'topic_id'=> 1,	
             'title' => 'RE',
-            'body' => 'hello we are  still dealing with issues!'
+            'body' => 'hello we are  still dealing with issues!',
+			'created_at' => Carbon::now()->format('Y-m-d H:i:s')	
         ]);
 		  DB::table('comments')->insert([
             'user_id' => 1,
             'topic_id'=> 2,	
             'title' => 'RE',
-            'body' => 'hello we are  still dealing with user status!'
+            'body' => 'hello we are  still dealing with user status!',
+			'created_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
 		 DB::table('comments')->insert([
             'user_id' => 1,
             'topic_id'=> 3,	
             'title' => 'gundam versus',
-            'body' => 'its a thing indeed'
+            'body' => 'its a thing indeed',  
+			'created_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
     }
 }
--- database/seeds/TopicTableSeeder.php
+++ database/seeds/TopicTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class TopicTableSeeder extends Seeder
 {
@@ -14,17 +15,19 @@ class TopicTableSeeder extends Seeder
           DB::table('topics')->insert([
             'user_id' => 1,
             'title' => 'Admin',
-            'body' => 'hello we are  dealing with new have a nice day!'
+            'body' => 'hello we are  dealing with new have a nice day!', 
+			'created_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
 		  DB::table('topics')->insert([
             'user_id' => 2,
             'title' => 'admin23 danounce',
-            'body' => 'hello we are  dealing with new have a nice day!'
+            'body' => 'hello we are  dealing with new have a nice day!',			    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
 		 DB::table('topics')->insert([
             'user_id' => 3,
             'title' => 'gundam versus',
-            'body' => 'its a thing!!'
+            'body' => 'its a thing!!',
+			'created_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
     }
 }
